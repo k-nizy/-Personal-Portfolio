2194d1802fffb38f77a142229ccd05a4386b24b1
k-nizy <k.nizeyiman@alustudent.com>
2025-08-17 14:06:41 +0200

    added the html ,css,and java

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-script.js
+const resumeBtns = document.querySelectorAll('.resume-btn');
+
+resumeBtns.forEach((btn, idx) => {
+    btn.addEventListener('click', () => {
+        const resumeDetails = document.querySelectorAll('.resume-details');
+        // Remove active class from all buttons
+        resumeBtns.forEach(btn => {
+            btn.classList.remove('active');
+        });
+        // Add active class to the clicked button
+        btn.classList.add('active');
+
+        resumeDetails.forEach(detail => {
+            detail.classList.remove('active');
+        });
+        resumeDetails[idx].classList.add('active');
+    });
+});
